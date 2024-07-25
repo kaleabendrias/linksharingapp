8a4afc469eef9b187e6dcc69fc7b010a9fd5382f
kaleabendrias <kaleabendrias1212@gmail.com>
2024-07-25 13:22:09 +0300

    refactor: Update Home component to handle authentication before rendering CustomizeLinksForm

--- devlinks-app/app/page.tsx
+++ devlinks-app/app/page.tsx
@@ -1,8 +1,21 @@
 import CustomizeLinksForm from "@/components/CustomizeLinksForm";
-import LinkPreview from "@/components/LinkPreview";
+import { getTokens } from "next-firebase-auth-edge";
+import { cookies } from "next/headers";
+import { clientConfig, serverConfig } from "../config";
+import { redirect } from "next/navigation";
 
-import Image from "next/image";
-
-export default function Home() {
+const Home = async () => {
+  const tokens = await getTokens(cookies(), {
+    apiKey: clientConfig.apiKey,
+    cookieName: serverConfig.cookieName,
+    cookieSignatureKeys: serverConfig.cookieSignatureKeys,
+    serviceAccount: serverConfig.serviceAccount,
+  });
+  if (!tokens) {
+    redirect("/auth/login");
+    return null;
+  }
   return <CustomizeLinksForm />;
-}
+};
+
+export default Home;
